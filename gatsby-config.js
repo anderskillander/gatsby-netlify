94f7c029836ec537e7e8d31e91f53fcefe5190f3
anderskillander <55491817+anderskillander@users.noreply.github.com>
2020-08-05 10:56:12 +0200

    changed path

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/content/posts/blog`,
       },
     },
   ],
